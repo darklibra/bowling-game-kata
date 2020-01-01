6fe3b616472e9cd56cd1698d2635abd9172ed28f
dain <darklibra25@gmail.com>
2020-01-01 13:26:00 +0900

    2020.01.01

--- src/main/java/com/kry/kata/bowling/BowlingGame.java
+++ src/main/java/com/kry/kata/bowling/BowlingGame.java
@@ -1,7 +1,53 @@
 package com.kry.kata.bowling;
 
 public class BowlingGame {
-    public void roll(final int pins) {
+    private final int MAX_FRAME = 10;
+    private final int MAX_SCORE_FOR_FRAME = 10;
 
+    private int rollNumber = 0;
+    private int[] rolls = new int[21];
+
+    public void roll(final int pins) {
+        this.rolls[rollNumber++] = pins;
+    }
+
+    public int score() {
+        var sum = 0;
+        var frame = 0;
+
+        for (int i = 0; i < MAX_FRAME; i++) {
+            if (isStrike(frame)) {
+                sum += MAX_SCORE_FOR_FRAME + strikeBonus(frame);
+                frame += 1;
+            } else if (isSpare(frame)) {
+                sum += MAX_SCORE_FOR_FRAME + spareBonus(frame);
+                frame += 2;
+            } else {
+                sum += frameScore(frame);
+                frame += 2;
+            }
+        }
+
+        return sum;
+    }
+
+    private int frameScore(final int roll) {
+        return rolls[roll] + rolls[roll + 1];
+    }
+
+    private int spareBonus(final int roll) {
+        return rolls[roll + 2];
+    }
+
+    private int strikeBonus(final int roll) {
+        return rolls[roll + 1] + rolls[roll + 2];
+    }
+
+    private boolean isSpare(final int roll) {
+        return frameScore(roll) == MAX_SCORE_FOR_FRAME;
+    }
+
+    private boolean isStrike(final int roll) {
+        return rolls[roll] == MAX_SCORE_FOR_FRAME;
     }
 }
--- src/test/java/com/kry/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kry/kata/bowling/BowlingGameTest.java
@@ -1,12 +1,65 @@
 package com.kry.kata.bowling;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+@DisplayName("볼링 게임 테스트")
 public class BowlingGameTest {
+
+    private BowlingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new BowlingGame();
+    }
+
+    @DisplayName("단일 값으로 점수 계산")
+    @ParameterizedTest(name = "{index} ==> times={0}, pins={1}, expected={2}")
+    @CsvSource({"20, 0, 0", "20, 1, 20", "20, 4, 80"})
+    public void rollManySinglePins(final int times, final int pins, final int expected) {
+        rollMany(times, pins);
+
+        Assertions.assertEquals(expected, game.score());
+    }
+
     @Test
-    @DisplayName("볼링 게임 생성 테스트")
-    public void created() {
-        new BowlingGame();
+    public void perfectGame() {
+        rollMany(12, 10);
+
+        Assertions.assertEquals(300, game.score());
+    }
+
+    @Test
+    @DisplayName("스페어 테스트")
+    public void spare() {
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+
+        rollMany(17, 0);
+
+        Assertions.assertEquals(16, game.score());
+    }
+
+    @Test
+    @DisplayName("스트라이크 테스트")
+    public void strike() {
+        game.roll(10);
+        game.roll(3);
+        game.roll(5);
+
+        rollMany(16, 0);
+
+        Assertions.assertEquals(26, game.score());
+    }
+
+    private void rollMany(final int times, final int pins) {
+        for (int i = 0; i < times; i++) {
+            game.roll(pins);
+        }
     }
 }
