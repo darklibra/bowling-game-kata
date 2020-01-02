4cbbf922d4f08a3f9373ea0f2d311fe3646dd7a5
dain <darklibra25@gmail.com>
2020-01-02 23:24:57 +0900

    1/2

--- src/main/java/com/kry/kata/bowling/BowlingGame.java
+++ src/main/java/com/kry/kata/bowling/BowlingGame.java
@@ -1,7 +1,157 @@
 package com.kry.kata.bowling;
 
 public class BowlingGame {
-    public void roll(final int pins) {
+    private Frames frames;
 
+    public BowlingGame() {
+        this.frames = new Frames();
+    }
+
+    public void roll(final int pins) {
+        this.frames.roll(pins);
+    }
+
+    public int score() {
+        return this.frames.score();
+
+    }
+
+    private static class Frames {
+        public Frame first;
+
+        public Frame current;
+
+        public Frames() {
+            this.first = new HeadFrame();
+            this.current = this.first;
+        }
+
+        public void roll(final int pins) {
+            this.current = this.current.roll(pins);
+        }
+
+        public int score() {
+            return this.first.score();
+        }
+    }
+
+    private static class Frame {
+        private static final Frame TERMINATE = new Frame(0) {
+            @Override
+            public Frame roll(final int pins) {
+                return this;
+            }
+
+            @Override
+            public int score() {
+                return 0;
+            }
+        };
+
+        protected int num;
+        protected int rest;
+        protected int first = 0;
+        protected int second = 0;
+
+        protected Frame next = TERMINATE;
+
+        public Frame(final int rest) {
+            this.num = 0;
+            this.rest = rest;
+        }
+
+        public Frame roll(final int pins) {
+            if (num == 0) {
+                this.first = pins;
+                this.num++;
+
+                return this;
+            } else if (!isStrike() && num == 1) {
+                this.second = pins;
+                this.num++;
+
+                return this;
+            } else {
+                return nextFrame(pins);
+            }
+        }
+
+        private Frame nextFrame(final int pins) {
+            this.next = (this.rest == 1) ? new LastFrame() : new Frame(this.rest - 1);
+
+            return this.next.roll(pins);
+        }
+
+        public int score() {
+            if (isStrike()) {
+                return 10 + this.strikeScore() + this.next.score();
+            }
+
+            final int frameScore = this.first + this.second;
+            if (frameScore == 10) {
+                return 10 + this.next.first + this.next.score();
+            } else {
+                return frameScore + this.next.score();
+            }
+        }
+
+        private int strikeScore() {
+            final Frame next = this.next;
+            if (next.isStrike()) {
+                return next.first + next.next.first;
+            }
+            return next.first + next.second;
+        }
+
+        protected boolean isStrike() {
+            return this.first == 10;
+        }
+    }
+
+    private static class HeadFrame extends Frame {
+        public HeadFrame() {
+            super(10);
+        }
+
+        @Override
+        public Frame roll(final int pins) {
+            this.next = new Frame(this.rest - 1);
+
+            return this.next.roll(pins);
+        }
+
+        @Override
+        public int score() {
+            return this.next.score();
+        }
+    }
+
+    private static class LastFrame extends Frame {
+        private int third = 0;
+
+        public LastFrame() {
+            super(0);
+        }
+
+        @Override
+        public Frame roll(final int pins) {
+            if (num == 0) {
+                this.first = pins;
+                this.num++;
+            } else if (num == 1) {
+                this.second = pins;
+                this.num++;
+            } else if (num == 2) {
+                this.third = pins;
+                this.num++;
+            }
+
+            return this;
+        }
+
+        @Override
+        public int score() {
+            return ((isStrike()) ? 20 : 0) + this.third + super.score();
+        }
     }
 }
--- src/test/java/com/kry/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kry/kata/bowling/BowlingGameTest.java
@@ -1,12 +1,78 @@
 package com.kry.kata.bowling;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
+
+    private BowlingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new BowlingGame();
+    }
+
     @Test
-    @DisplayName("볼링 게임 생성 테스트")
-    public void created() {
-        new BowlingGame();
+    @DisplayName("전체 프레임에서 쓰러트린 핀수 0인 경우")
+    public void allZero() {
+        many(20, 0);
+
+        Assertions.assertEquals(0, game.score());
+    }
+
+    @Test
+    @DisplayName("전체 프레임에서 쓰러트린 핀수 1인 경우")
+    public void allOne() {
+        many(20, 1);
+
+        Assertions.assertEquals(20, game.score());
+    }
+
+    @Test
+    @DisplayName("스페어 처리 테스트")
+    public void spare() {
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+
+        many(17, 0);
+
+        Assertions.assertEquals(16, game.score());
+    }
+
+    @Test
+    @DisplayName("스트라이크 처리 테스트")
+    public void strike() {
+        game.roll(10); // strike
+        game.roll(3);
+        game.roll(5);
+        many(26, 0);
+
+        Assertions.assertEquals(26, game.score());
+    }
+
+    @Test
+    @DisplayName("11회 연속 스트라이크")
+    public void elevenBagger() throws Exception {
+        many(11, 10);
+        game.roll(3);
+
+        Assertions.assertEquals(293, game.score());
+    }
+
+    @Test
+    @DisplayName("퍼펙트 게임 테스트")
+    public void perfectGame() throws Exception {
+        many(12, 10);
+
+        Assertions.assertEquals(300, game.score());
+    }
+
+    private void many(final int times, final int pins) {
+        for (int i =     0; i < times; i++) {
+            game.roll(pins);
+        }
     }
 }
