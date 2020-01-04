ccf28bb5b150ed7f7477c1c55633fdea3e012885
dain <darklibra25@gmail.com>
2020-01-04 18:52:36 +0900

    2020.01.04

--- src/main/java/com/kry/kata/bowling/BowlingGame.java
+++ src/main/java/com/kry/kata/bowling/BowlingGame.java
@@ -1,7 +1,43 @@
 package com.kry.kata.bowling;
 
 public class BowlingGame {
-    public void roll(final int pins) {
+    private static final int MAX_SCORE_IN_FRAME = 10;
 
+    private int[] pins = new int[23];
+    private int num;
+
+    public void roll(final int pins) {
+        this.pins[num++] = pins;
+    }
+
+    public int score() {
+        var sum = 0;
+        var boll = 0;
+
+        for (int i = 0; i < 10; i++) {
+            if (isStrike(boll)) {
+                sum += MAX_SCORE_IN_FRAME + nextTwoBollsForStrike(boll);
+
+                boll++;
+            } else {
+                sum += (isSpare(boll)) ? MAX_SCORE_IN_FRAME + pins[boll + 2]:(pins[boll] + pins[boll + 1]);
+
+                boll += 2;
+            }
+        }
+
+        return sum;
+    }
+
+    private int nextTwoBollsForStrike(final int boll) {
+        return pins[boll + 1] + pins[boll + 2];
+    }
+
+    private boolean isStrike(final int boll) {
+        return pins[boll]==10;
+    }
+
+    private boolean isSpare(final int boll) {
+        return (pins[boll] + pins[boll + 1])==10;
     }
 }
--- src/test/java/com/kry/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kry/kata/bowling/BowlingGameTest.java
@@ -1,12 +1,81 @@
 package com.kry.kata.bowling;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
+    private BowlingGame g;
+
+    @BeforeEach
+    void setUp() {
+        g = new BowlingGame();
+    }
+
     @Test
-    @DisplayName("볼링 게임 생성 테스트")
-    public void created() {
-        new BowlingGame();
+    @DisplayName("전체 프레임이 0인 경우")
+    public void rollAllZeroTest() {
+        rollMany(20, 0);
+
+        Assertions.assertEquals(0, g.score());
+    }
+
+    @Test
+    @DisplayName("전체 프레임이 1인 경우")
+    public void rollAllOneTest() {
+        rollMany(20, 1);
+
+        Assertions.assertEquals(20, g.score());
+    }
+
+    @Test
+    @DisplayName("스페어 테스트")
+    public void spareTest() {
+        g.roll(4);
+        g.roll(6);
+        g.roll(3);
+
+        rollMany(17, 0);
+
+        Assertions.assertEquals(16, g.score());
+    }
+
+    @Test
+    @DisplayName("스트라이크 테스트")
+    public void strikeTest() {
+        g.roll(10);
+        g.roll(3);
+        g.roll(3);
+
+        rollMany(16, 0);
+
+        Assertions.assertEquals(22, g.score());
+    }
+
+    @Test
+    @DisplayName("더블 스트라이크 테스트")
+    public void doubleStrikeTest() {
+        g.roll(10);
+        g.roll(10);
+        g.roll(3);
+
+        rollMany(16, 0);
+
+        Assertions.assertEquals(39, g.score());
+    }
+
+    @Test
+    @DisplayName("PerfectGame")
+    public void perfectGame() {
+        rollMany(12, 10);
+
+        Assertions.assertEquals(300, g.score());
+    }
+
+    private void rollMany(final int times, final int pins) {
+        for (int i = 0; i < times; i++) {
+            g.roll(pins);
+        }
     }
 }
