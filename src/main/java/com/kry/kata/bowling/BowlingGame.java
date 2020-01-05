3e1a493e64b8167a09a1146a46ae2bc365321b22
dain <darklibra25@gmail.com>
2020-01-05 22:05:47 +0900

    2020.01.05

--- src/main/java/com/kry/kata/bowling/BowlingGame.java
+++ src/main/java/com/kry/kata/bowling/BowlingGame.java
@@ -1,7 +1,75 @@
 package com.kry.kata.bowling;
 
 public class BowlingGame {
-    public void roll(final int pins) {
+    private final int MAX_FRAME_COUNT = 10;
 
+    private int pins[] = new int[21];
+
+    private ScoreWriter writer;
+    private ScoreCalculator calculator;
+    private FrameStatusChecker checker;
+
+    public BowlingGame() {
+        this.writer = new ScoreWriter();
+        this.calculator = new ScoreCalculator();
+        this.checker = new FrameStatusChecker();
+    }
+
+    public void roll(final int pins) {
+        this.writer.roll(pins);
+    }
+
+    public int score() {
+        var score = 0;
+        var boll = 0;
+
+        for (int i = 0; i < MAX_FRAME_COUNT; i++) {
+            if (checker.strike(boll)) {
+                score += calculator.strike(boll);
+                boll++;
+            } else if (checker.spare(boll)) {
+                score += calculator.spare(boll);
+                boll += 2;
+            } else {
+                score += calculator.frameScore(boll);
+                boll += 2;
+            }
+        }
+
+        return score;
+    }
+
+    class ScoreWriter {
+        private int num = 0;
+
+        public void roll(final int pins) {
+            BowlingGame.this.pins[num++] = pins;
+        }
+    }
+
+    class ScoreCalculator {
+        int strike(final int boll) {
+            return 10 + pins[boll + 1] + pins[boll + 2];
+        }
+
+        int spare(final int boll) {
+            return 10 + pins[boll + 2];
+        }
+
+        int frameScore(final int boll) {
+            return pins[boll] + pins[boll + 1];
+        }
+    }
+
+    class FrameStatusChecker {
+        private final int MAX_SCORE_IN_FRAME = 10;
+
+        public boolean strike(final int boll) {
+            return pins[boll]==MAX_SCORE_IN_FRAME;
+        }
+
+        public boolean spare(final int boll) {
+            return (pins[boll] + pins[boll + 1])==MAX_SCORE_IN_FRAME;
+        }
     }
 }
--- src/test/java/com/kry/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kry/kata/bowling/BowlingGameTest.java
@@ -1,12 +1,65 @@
 package com.kry.kata.bowling;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class BowlingGameTest {
+
+    private BowlingGame g;
+
+    @BeforeEach
+    void setUp() {
+        g = new BowlingGame();
+    }
+
+    @ParameterizedTest(name = "{index} => all pins : {0}")
+    @DisplayName("동일한 점수로 20번 점수 기록 테스트")
+    @CsvSource({"0, 0", "1, 20", "2, 40", "3, 60", "4, 80"})
+    public void times20AndSamePins(final int pins, final int expected) {
+        manyRoll(20, pins);
+
+        Assertions.assertEquals(expected, g.score());
+    }
+
     @Test
-    @DisplayName("볼링 게임 생성 테스트")
-    public void created() {
-        new BowlingGame();
+    @DisplayName("스페어")
+    public void spare() {
+        g.roll(7);
+        g.roll(3);
+        g.roll(3);
+
+        manyRoll(17, 0);
+
+        Assertions.assertEquals(16, g.score());
+    }
+
+    @Test
+    @DisplayName("스트라이크")
+    public void strike() {
+        g.roll(10);
+        g.roll(3);
+        g.roll(3);
+
+        manyRoll(16, 0);
+
+        Assertions.assertEquals(22, g.score());
+    }
+
+    @Test
+    @DisplayName("퍼펙트 게임")
+    public void perfectGame() {
+        manyRoll(12, 10);
+
+        Assertions.assertEquals(300, g.score());
+    }
+
+    private void manyRoll(final int times, final int pins) {
+        for (int i = 0; i < times; i++) {
+            g.roll(pins);
+        }
     }
 }
