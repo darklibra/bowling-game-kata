0a6ee5b7dcccbe58768b3a22f09e97c901f1d49e
dain <darklibra25@gmail.com>
2020-01-16 19:49:08 +0900

    2020.01.16

--- src/main/java/com/kry/kata/bowling/BowlingGame.java
+++ src/main/java/com/kry/kata/bowling/BowlingGame.java
@@ -1,7 +1,66 @@
 package com.kry.kata.bowling;
 
 public class BowlingGame {
-    public void roll(final int pins) {
+    private int[] pins = new int[21];
 
+    private ScoreWriter writer = new ScoreWriter();
+    private ScoreCalculator calculator = new ScoreCalculator();
+
+    public void roll(final int pins) {
+        writer.write(pins);
+    }
+
+    public int score() {
+        return calculator.cal();
+    }
+
+    private class ScoreWriter {
+        private int num = 0;
+
+        public void write(final int pins) {
+            BowlingGame.this.pins[num++] = pins;
+        }
+    }
+
+    private class ScoreCalculator {
+        public int cal() {
+            int score = 0;
+            int boll = 0;
+
+            for (int i = 0; i < 10; i++) {
+                if (strike(boll)) {
+                    score += 10 + nextTwoBollsForStrike(boll);
+                    boll += 1;
+                    continue;
+                }
+
+                final int fs = frameScoreAt(boll);
+
+                score += fs;
+                if (fs==10) {
+                    score += nextOneBollForSpare(boll);
+                }
+
+                boll += 2;
+            }
+
+            return score;
+        }
+
+        private int nextOneBollForSpare(final int boll) {
+            return pins[boll + 2];
+        }
+
+        private int nextTwoBollsForStrike(final int boll) {
+            return pins[boll + 1] + pins[boll + 2];
+        }
+
+        private int frameScoreAt(final int boll) {
+            return pins[boll] + pins[boll + 1];
+        }
+
+        private boolean strike(final int boll) {
+            return pins[boll]==10;
+        }
     }
 }
--- src/test/java/com/kry/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kry/kata/bowling/BowlingGameTest.java
@@ -1,12 +1,69 @@
 package com.kry.kata.bowling;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
+    private BowlingGame g;
+
+    @BeforeEach
+    void setUp() {
+        g = new BowlingGame();
+    }
+
     @Test
-    @DisplayName("볼링 게임 생성 테스트")
-    public void created() {
-        new BowlingGame();
+    @DisplayName("10 프레임 점수가 0인 테스트")
+    public void all_frames_score_is_zero() {
+        rollMany(20, 0);
+
+        Assertions.assertEquals(0, g.score());
+    }
+
+    @Test
+    @DisplayName("10 프레임 점수가 1인 테스트")
+    public void all_frames_score_is_one() {
+        rollMany(20, 1);
+
+        Assertions.assertEquals(20, g.score());
+    }
+
+    @Test
+    @DisplayName("1 스페어")
+    public void one_spare() {
+        g.roll(7);
+        g.roll(3);
+        g.roll(3);
+
+        rollMany(17, 0);
+
+        Assertions.assertEquals((7 + 3 + 3) + 3, g.score());
+    }
+
+    @Test
+    @DisplayName("1 스트라이크")
+    public void one_strike() {
+        g.roll(10);
+        g.roll(3);
+        g.roll(3);
+
+        rollMany(16, 0);
+
+        Assertions.assertEquals((10 + 3 + 3) + 3 + 3, g.score());
+    }
+
+    @Test
+    @DisplayName("퍼펙트 게임")
+    public void perfect_game() {
+        rollMany(12, 10);
+
+        Assertions.assertEquals(300, g.score());
+    }
+
+    private void rollMany(final int times, final int pins) {
+        for (int i = 0; i < times; i++) {
+            g.roll(pins);
+        }
     }
 }
