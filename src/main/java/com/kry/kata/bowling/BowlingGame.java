424d08c0ff0658794b4eff791c299a10c7eec527
dain <darklibra25@gmail.com>
2020-01-19 22:16:11 +0900

    2020.01.19

--- src/main/java/com/kry/kata/bowling/BowlingGame.java
+++ src/main/java/com/kry/kata/bowling/BowlingGame.java
@@ -1,7 +1,55 @@
 package com.kry.kata.bowling;
 
 public class BowlingGame {
-    public void roll(final int pins) {
+    private int[] pins = new int[21];
+    private int num = 0;
 
+    public void roll(final int pins) {
+        this.pins[num++] = pins;
+    }
+
+    public int score() {
+        int score = 0;
+        int boll = 0;
+
+        final FrameCond cond = new FrameCond();
+
+        for (int i = 0; i < 10; i++) {
+            final boolean strike = cond.isStrike(boll);
+
+            if (strike) {
+                score += 10 + nextTwoBollForStrike(boll);
+            } else if (cond.isSpare(boll)) {
+                score += 10 + nextOneBollForSpare(boll);
+            } else {
+                score += scoreForFrame(boll);
+            }
+
+            boll += (strike) ? 1:2;
+        }
+
+        return score;
+    }
+
+    private int nextTwoBollForStrike(final int boll) {
+        return pins[boll + 1] + pins[boll + 2];
+    }
+
+    private int nextOneBollForSpare(final int boll) {
+        return pins[boll + 2];
+    }
+
+    private int scoreForFrame(final int boll) {
+        return pins[boll] + pins[boll + 1];
+    }
+
+    class FrameCond {
+        boolean isStrike(final int boll) {
+            return pins[boll]==10;
+        }
+
+        boolean isSpare(final int boll) {
+            return pins[boll] + pins[boll + 1]==10;
+        }
     }
 }
--- src/test/java/com/kry/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kry/kata/bowling/BowlingGameTest.java
@@ -1,12 +1,73 @@
 package com.kry.kata.bowling;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
+    private BowlingGame g;
+
+    @BeforeEach
+    void setUp() {
+        g = new BowlingGame();
+    }
+
     @Test
-    @DisplayName("볼링 게임 생성 테스트")
-    public void created() {
-        new BowlingGame();
+    @DisplayName("전체 프레임 점수 0")
+    public void allFrameScoreZro() {
+        manyTimes(20, 0);
+
+        Assertions.assertEquals(0, g.score());
+    }
+
+    @Test
+    @DisplayName("전체 프레임 점수 1")
+    public void allFrameScoreOne() {
+        manyTimes(20, 1);
+
+        Assertions.assertEquals(20, g.score());
+    }
+
+    @Test
+    @DisplayName("스페어 테스트")
+    public void 스페어_테스트() {
+        g.roll(7);
+        g.roll(3);
+        g.roll(3);
+
+        manyTimes(17, 0);
+
+        Assertions.assertEquals((7 + 3 + 3) + 3, g.score());
+    }
+
+    @Test
+    @DisplayName("스트라이크 테스트")
+    public void 스트라이크_테스트() {
+        g.roll(7);
+        g.roll(3);
+        g.roll(3);
+
+        manyTimes(17, 0);
+
+        Assertions.assertEquals((7 + 3 + 3) + 3, g.score());
+    }
+
+    @Test
+    @DisplayName("퍼펙트 게임 테스트")
+    public void 퍼펙트_게임_테스트() {
+        g.roll(7);
+        g.roll(3);
+        g.roll(3);
+
+        manyTimes(17, 0);
+
+        Assertions.assertEquals((7 + 3 + 3) + 3, g.score());
+    }
+
+    private void manyTimes(final int times, final int pins) {
+        for (int i = 0; i < times; i++) {
+            g.roll(pins);
+        }
     }
 }
