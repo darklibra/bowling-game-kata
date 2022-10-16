b85cb45021aa0de73ca4a83eef8e7824e7563624
“dain” <darklibra25@gmail.com>
2022-10-16 22:19:53 +0900

    2022.10.16

--- src/main/java/com/kry/kata/bowling/BowlingGame.java
+++ src/main/java/com/kry/kata/bowling/BowlingGame.java
@@ -1,7 +1,53 @@
 package com.kry.kata.bowling;
 
 public class BowlingGame {
-    public void roll(final int pins) {
+    private int[] score = new int[21];
+    private int pos = 0;
 
+    public void roll(final int pins) {
+        this.score[pos] = pins;
+        pos++;
+    }
+
+    public int score() {
+        int total = 0;
+
+        for (int pt = 0; pt < pos; ) {
+            total += calFrameScore(pt);
+
+            pt += (isStrike(pt) ? 1 : 2);
+        }
+
+        return total;
+    }
+
+    private int calFrameScore(int position) {
+        if (isStrike(position)) {
+            return getStrikeScoreAt(position);
+        } else if (isSpare(position)) {
+            return getSpareScoreAt(position);
+        }
+
+        return getFrameScoreAt(position);
+    }
+
+    private int getSpareScoreAt(int position) {
+        return 10 + this.score[position + 2];
+    }
+
+    private int getStrikeScoreAt(int position) {
+        return 10 + score[position + 1] + score[position + 2];
+    }
+
+    private boolean isStrike(int position) {
+        return score[position] == 10;
+    }
+
+    private boolean isSpare(int position) {
+        return getFrameScoreAt(position) == 10;
+    }
+
+    private int getFrameScoreAt(int position) {
+        return score[position] + score[position + 1];
     }
 }
--- src/test/java/com/kry/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kry/kata/bowling/BowlingGameTest.java
@@ -1,12 +1,78 @@
 package com.kry.kata.bowling;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class BowlingGameTest {
-    @Test
-    @DisplayName("볼링 게임 생성 테스트")
-    public void created() {
-        new BowlingGame();
+
+    private BowlingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new BowlingGame();
     }
+
+    @Nested
+    class OneTime {
+        @Test
+        @DisplayName("0점 롤링 테스트")
+        public void rolledZero() {
+            game.roll(0);
+
+            Assertions.assertEquals(0, game.score());
+        }
+
+        @Test
+        @DisplayName("1점 롤링 테스트")
+        public void rolledOne() {
+            game.roll(1);
+
+            Assertions.assertEquals(1, game.score());
+        }
+    }
+
+    @Nested
+    class Times {
+        @Test
+        @DisplayName("1점 롤링 테스트")
+        public void rolledOne() {
+            times(20, 1);
+
+            Assertions.assertEquals(20, game.score());
+        }
+
+        @Test
+        @DisplayName("스페어 테스트")
+        public void spare() {
+            game.roll(5);
+            game.roll(5);
+            game.roll(3);
+
+            Assertions.assertEquals(16, game.score());
+        }
+
+        @Test
+        @DisplayName("스트라이크 테스트")
+        public void strike() {
+            game.roll(10);
+            game.roll(5);
+            game.roll(4);
+
+            Assertions.assertEquals(28, game.score());
+        }
+
+        @Test
+        @DisplayName("퍼펙트 테스트")
+        public void perfect() {
+            times(11, 10);
+
+            Assertions.assertEquals(300, game.score());
+        }
+
+        private void times(int times, int score) {
+            for (int i = 0; i < times; i++) {
+                game.roll(score);
+            }
+        }
+    }
+
 }
