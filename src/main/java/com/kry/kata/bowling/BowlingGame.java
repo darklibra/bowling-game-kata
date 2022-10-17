8bf128b13a3998baca77e4e8cfdcc38adb060928
dain <darklibra24@gmail.com>
2022-10-17 10:27:36 +0000

    2022.10.17

--- src/main/java/com/kry/kata/bowling/BowlingGame.java
+++ src/main/java/com/kry/kata/bowling/BowlingGame.java
@@ -1,7 +1,47 @@
 package com.kry.kata.bowling;
 
 public class BowlingGame {
-    public void roll(final int pins) {
+  private static final int MAX_PINS = 10;
 
+  private int score[] = new int[21];
+  private int cur = 0;
+
+  public void roll(final int pins) {
+    this.score[this.cur] = pins;
+    this.cur += 1;
+  }
+
+  public Integer score() {
+    int score = 0;
+
+    for (int i = 0; i < this.cur;) {
+      score += calFrameScoreAt(i);
+
+      i += (isStrike(i) ? 1 : 2);
     }
+
+    return score;
+  }
+
+  private int calFrameScoreAt(int pos) {
+    if (isStrike(pos)) {
+      return MAX_PINS + this.score[pos + 1] + this.score[pos + 2];
+    } else if (isSpare(pos)) {
+      return MAX_PINS + this.score[pos + 2];
+    }
+
+    return getFrameScore(pos);
+  }
+
+  private boolean isSpare(int pos) {
+    return getFrameScore(pos) == MAX_PINS;
+  }
+
+  private int getFrameScore(int pos) {
+    return this.score[pos] + this.score[pos + 1];
+  }
+
+  private boolean isStrike(int pos) {
+    return this.score[pos] == MAX_PINS;
+  }
 }
--- src/test/java/com/kry/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kry/kata/bowling/BowlingGameTest.java
@@ -1,12 +1,70 @@
 package com.kry.kata.bowling;
 
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
+    private BowlingGame game;
+
+    @BeforeEach
+    public void init() {
+      game = new BowlingGame();
+    }
+
     @Test
-    @DisplayName("볼링 게임 생성 테스트")
-    public void created() {
-        new BowlingGame();
+    @DisplayName("0점 롤링 테스트")
+    public void rollZero() {
+      game.roll(0);
+
+      Assertions.assertEquals(0, game.score());
+    }
+
+    @Test
+    @DisplayName("1점 롤링 테스트")
+    public void rolledOne() {
+      game.roll(1);
+
+      Assertions.assertEquals(1, game.score());
+    }
+
+    @Test
+    @DisplayName("spare")
+    public void spare() {
+      game.roll(3);
+      game.roll(7);
+      game.roll(3);
+      game.roll(3);
+
+      Assertions.assertEquals(19, game.score());
+    }
+
+    @Test
+    @DisplayName("strike")
+    public void strike() {
+      game.roll(10);
+      game.roll(5);
+      game.roll(3);
+      game.roll(3);
+
+      Assertions.assertEquals(29, game.score());
+    }
+
+    @Test
+    @DisplayName("퍼펙트 테스트")
+    public void prefect() {
+      times(11, 10);
+
+      Assertions.assertEquals(300, game.score());
+    }
+
+    private void times(int times, int pins) {
+      IntStream.range(0, times)
+      .forEach(idx -> {
+        this.game.roll(pins);
+      });;
     }
 }
