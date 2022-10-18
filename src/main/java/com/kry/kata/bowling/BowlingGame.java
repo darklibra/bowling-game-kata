3a8e2aca3f5016baffae993295ed21bc1befe8c7
dain <darklibra24@gmail.com>
2022-10-18 12:00:22 +0000

    2/3

--- src/main/java/com/kry/kata/bowling/BowlingGame.java
+++ src/main/java/com/kry/kata/bowling/BowlingGame.java
@@ -1,7 +1,54 @@
 package com.kry.kata.bowling;
 
 public class BowlingGame {
-    public void roll(final int pins) {
+    private int[] score = new int[21];
+    private int cur = 0;
 
+    public void roll(final int pins) {
+      this.score[cur] = pins;
+      this.cur += 1;
+    }
+
+    public Integer score() {
+        int score = 0;
+        for (int i = 0; i < this.cur; ) {
+          int moveTo = 0;
+          if (isStrikeAt(i)) {
+            score += getStrikeScore(i);
+
+            moveTo = 1;
+          } else {
+            score += getFrameAndSpareScore(i);
+
+            moveTo = 2;
+          }
+
+          i += moveTo;
+        }
+
+        return score;
+    }
+
+    private int getStrikeScore(int i) {
+      return 10 + this.score[i + 1] + this.score[i + 2];
+    }
+
+    private int getFrameAndSpareScore(int i) {
+      int frame = frameScoreAt(i);
+
+      if (frame == 10) {
+        return 10 + this.score[i + 2];
+      }
+
+      return frame;
+    }
+
+    private int frameScoreAt(int at) {
+      return this.score[at] + this.score[at + 1];
+    }
+
+    private boolean isStrikeAt(int at) {
+      return this.score[at] == 10;
     }
 }
+
--- src/test/java/com/kry/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kry/kata/bowling/BowlingGameTest.java
@@ -1,12 +1,64 @@
 package com.kry.kata.bowling;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
+    private BowlingGame game;
+
+    @BeforeEach
+    public void init() {
+      this.game = new BowlingGame();
+    }
+
     @Test
-    @DisplayName("볼링 게임 생성 테스트")
-    public void created() {
-        new BowlingGame();
+    @DisplayName("0점 투구 테스트")
+    public void rollWithZeroScore() {
+        game.roll(0);
+
+        Assertions.assertEquals(0, game.score());
+    }
+
+    @Test
+    @DisplayName("1점 투구 테스트")
+    public void rollWithOneScore() {
+        game.roll(1);
+
+        Assertions.assertEquals(1, game.score());
+    }
+
+    @Test
+    @DisplayName("spare")
+    public void spare() {
+        game.roll(3);
+        game.roll(7);
+        game.roll(5);
+        game.roll(3);
+
+        Assertions.assertEquals(23, game.score());
+    }
+
+    @Test
+    @DisplayName("strike")
+    public void strike() {
+        game.roll(10);
+        game.roll(7);
+        game.roll(1);
+        game.roll(1);
+
+        Assertions.assertEquals(27, game.score());
+    }
+
+    @Test
+    @DisplayName("perfect")
+    public void perfect() {
+        for (int i = 0; i < 11; i++) {
+          game.roll(10);
+        }
+
+        Assertions.assertEquals(300, game.score());
     }
 }
+
