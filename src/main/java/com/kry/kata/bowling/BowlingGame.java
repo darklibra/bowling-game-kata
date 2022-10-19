64da4105bcae60806a1783e786ef9371d5410042
“dain” <darklibra25@gmail.com>
2022-10-20 00:00:44 +0900

    2/4

--- src/main/java/com/kry/kata/bowling/BowlingGame.java
+++ src/main/java/com/kry/kata/bowling/BowlingGame.java
@@ -1,7 +1,45 @@
 package com.kry.kata.bowling;
 
 public class BowlingGame {
-    public void roll(final int pins) {
 
+    private int[] pins = new int[21];
+    private int cur = 0;
+
+    public void roll(final int pins) {
+        this.pins[cur] = pins;
+        this.cur += 1;
+
+    }
+
+    public int score() {
+        int score = 0;
+
+        for (int i = 0; i < cur; ) {
+            int moveTo = isStrike(i) ? 1 : 2;
+
+            score += calFrameScore(i);
+
+            i += moveTo;
+        }
+
+        return score;
+    }
+
+    private int calFrameScore(int at) {
+        if (isStrike(at)) {
+            return 10 + this.pins[at + 1] + this.pins[at + 2];
+        }
+
+        final int frameScore = this.pins[at] + this.pins[at + 1];
+
+        if (frameScore == 10) {
+            return 10 + this.pins[at + 2];
+        }
+
+        return frameScore;
+    }
+
+    private boolean isStrike(int i) {
+        return this.pins[i] == 10;
     }
 }
--- src/test/java/com/kry/kata/bowling/BowlingGameTest.java
+++ src/test/java/com/kry/kata/bowling/BowlingGameTest.java
@@ -1,12 +1,78 @@
 package com.kry.kata.bowling;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import kotlin.ranges.IntRange;
+import org.junit.jupiter.api.*;
+
+import java.util.stream.IntStream;
 
 public class BowlingGameTest {
-    @Test
-    @DisplayName("볼링 게임 생성 테스트")
-    public void created() {
-        new BowlingGame();
+
+    private BowlingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new BowlingGame();
+    }
+
+    @Nested
+    class OneTime {
+        @Test
+        @DisplayName("1회 투구: 1점")
+        public void onl() {
+            game.roll(1);
+
+            Assertions.assertEquals(1, game.score());
+        }
+    }
+
+    @Nested
+    class Times {
+        @Test
+        @DisplayName("다회 투구: 0점")
+        public void zero() {
+            times(0, 20);
+
+            Assertions.assertEquals(0, game.score());
+        }
+
+        @Test
+        @DisplayName("다회 투구: 1점")
+        public void one() {
+            times(1, 20);
+
+            Assertions.assertEquals(20, game.score());
+        }
+
+        @Test
+        @DisplayName("다회 투구: spare")
+        public void spare() {
+            game.roll(3);
+            game.roll(7);
+            times(1, 18);
+
+            Assertions.assertEquals(29, game.score());
+        }
+
+        @Test
+        @DisplayName("다회 투구: strike")
+        public void strike() {
+            game.roll(10);
+            times(1, 18);
+
+            Assertions.assertEquals(30, game.score());
+        }
+
+        @Test
+        @DisplayName("다회 투구: 퍼펙트")
+        public void perfect() {
+            times(10, 11);
+
+            Assertions.assertEquals(300, game.score());
+        }
+
+        private void times(int pins, int times) {
+            IntStream.range(0, times)
+                .forEach(idx -> game.roll(pins));
+        }
     }
 }
